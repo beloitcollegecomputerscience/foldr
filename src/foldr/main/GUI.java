f98037bd843c2967f85195fb06e97656f060cdf8
Nathanaël COURET <couretn@beloit.edu>
2014-05-06 22:22:56 -0500

    Removed menu elements from the GUI class and added a field of type MenuBar instead. it's working pretty well.

--- src/foldr/main/GUI.java
+++ src/foldr/main/GUI.java
@@ -83,24 +83,8 @@ public final class GUI extends JFrame
     // Capture the mouse location during drag events
     Point                              mouseDragLocation = null;
 
-    // menu components
-    private JMenuBar                   menuBar;
-    private JMenu                      fileMenu, editMenu, foldingMenu, windowMenu, helpMenu;
-    private JMenuItem                  fileOpen, fileNew, fileSave, fileSaveAs, fileExport,
-                    fileClose;
-    private JMenuItem                  editCopy, editCut, editPaste, editDelete, editSelectAll;
-    private JMenu                      foldingAngle, foldingShape;
-    private JMenuItem                  foldingThirty, foldingFortyFive, foldingNinety,
-                    foldingCustomAngle, foldingEdgeSelect, foldingPointSelect, foldingFoldShapes,
-                    foldingConnectShapes, foldingDetachShapes, foldingResizeShape;
-    private JMenu                      windowView, windowPerspective;
-    private JMenuItem                  windowShowTop, windowShowBack, windowShowLeft,
-                    windowShowHideTools, windowShowHideInfo, windowChangePerspective,
-                    windowSavePerspective, windowLoadPerspective, windowResizePerspective;
-    private JMenu                      helpLanguage;
-    private JMenuItem                  helpManual, helpQuickStartGuide;
-    private ButtonGroup                langGroup;
-    private List<JRadioButtonMenuItem> liLanguages;
+    // menu component
+    private MenuBar menuBar;
 
     ActionManager                      actionManager     = new ActionManager();
 
@@ -121,219 +105,6 @@ public final class GUI extends JFrame
     public GUI(String title) {
 
         super(title);
-    }
-
-    /**
-     * <p>
-     * Initialize the menu components and add them to the frame.
-     * <p>
-     * Menu structure :
-     * <p>
-     * File<br>
-     * |-Open<br>
-     * |-New<br>
-     * |-Save<br>
-     * |-Save As<br>
-     * |-Export<br>
-     * |-Close
-     * <p>
-     * Edit<br>
-     * |-Copy<br>
-     * |-Cut<br>
-     * |-Paste<br>
-     * |-Delete<br>
-     * |-Select All
-     * <p>
-     * Fold<br>
-     * |-Angle<br>
-     * | |-30<br>
-     * | |-45<br>
-     * | |-90<br>
-     * | |-Custom<br>
-     * |-Edge Select<br>
-     * |-Point Select<br>
-     * |-Shape<br>
-     * | |-Fold<br>
-     * | |-Connect<br>
-     * | |-Detach<br>
-     * | |-Resize
-     * <p>
-     * Window<br>
-     * |-View<br>
-     * | |-Top<br>
-     * | |-Back<br>
-     * | |-Left<br>
-     * |-Show/Hide Tool<br>
-     * |-Show/Hide Info<br>
-     * |-Perspective<br>
-     * | |-Change Perspective<br>
-     * | |-Save<br>
-     * | |-Load<br>
-     * | |-Resize
-     * <p>
-     * Help |-Manual<br>
-     * |-Quick Start Guide<br>
-     * |-Language<br>
-     * | |-<tt>List of languages</tt>
-     */
-    private void initMenuBarPane() {
-
-        menuBar = new JMenuBar();
-        // File submenu
-        fileMenu = new JMenu("File");
-        menuBar.add(fileMenu);// fileMenu -> menuBar
-        fileNew = new JMenuItem("New");
-        fileNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.META_MASK));
-        fileNew.addActionListener(this);
-        fileMenu.add(fileNew);
-        fileOpen = new JMenuItem("Open");
-        fileOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.META_MASK));
-        fileOpen.addActionListener(this);
-        fileMenu.add(fileOpen);
-        fileSave = new JMenuItem("Save");
-        fileSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.META_MASK));
-        fileSave.addActionListener(this);
-        fileMenu.add(fileSave);
-        fileSaveAs = new JMenuItem("Save As");
-        fileSaveAs.addActionListener(this);
-        fileMenu.add(fileSaveAs);
-        fileExport = new JMenuItem("Export");
-        fileExport.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.META_MASK));
-        fileExport.addActionListener(this);
-        fileMenu.add(fileExport);
-        fileClose = new JMenuItem("Close");
-        fileClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.META_MASK));
-        fileClose.addActionListener(this);
-        fileMenu.add(fileClose);
-
-        // Edit submenu
-        editMenu = new JMenu("Edit");
-        menuBar.add(editMenu); // editMenu -> menuBar
-        editCopy = new JMenuItem("Copy");
-        editCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.META_MASK));
-        editCopy.addActionListener(this);
-        editMenu.add(editCopy);
-        editCut = new JMenuItem("Cut");
-        editCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.META_MASK));
-        editCut.addActionListener(this);
-        editMenu.add(editCut);
-        editPaste = new JMenuItem("Paste");
-        editPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.META_MASK));
-        editPaste.addActionListener(this);
-        editMenu.add(editPaste);
-        editMenu.add(new JSeparator()); // separate
-        editDelete = new JMenuItem("Delete");
-        editDelete.setAccelerator(KeyStroke.getKeyStroke(
-            KeyEvent.VK_BACK_SPACE, ActionEvent.META_MASK));
-        editDelete.addActionListener(this);
-        editMenu.add(editDelete);
-        editSelectAll = new JMenuItem("Select All");
-        editSelectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.META_MASK));
-        editSelectAll.addActionListener(this);
-        editMenu.add(editSelectAll);
-
-        // Folding/Shapes submenu
-        foldingMenu = new JMenu("Folding");
-        menuBar.add(foldingMenu); // folMenu -> menuBar
-        foldingAngle = new JMenu("Angle");
-        foldingMenu.add(foldingAngle);
-        foldingThirty = new JMenuItem("Rotate 30 Degrees");
-        foldingThirty.addActionListener(this);
-        foldingAngle.add(foldingThirty);
-        foldingFortyFive = new JMenuItem("Rotate 45 Degrees");
-        foldingFortyFive.addActionListener(this);
-        foldingAngle.add(foldingFortyFive);
-        foldingNinety = new JMenuItem("Rotate 90 Degrees");
-        foldingNinety.addActionListener(this);
-        foldingAngle.add(foldingNinety);
-        foldingCustomAngle = new JMenuItem("Custom Angle");
-        foldingCustomAngle.addActionListener(this);
-        foldingAngle.add(foldingCustomAngle);
-        foldingMenu.add(new JSeparator()); // separate
-        foldingEdgeSelect = new JMenuItem("Edge Select");
-        foldingEdgeSelect.addActionListener(this);
-        foldingMenu.add(foldingEdgeSelect);
-        foldingPointSelect = new JMenuItem("Point Select");
-        foldingPointSelect.addActionListener(this);
-        foldingMenu.add(foldingPointSelect);
-        foldingMenu.add(new JSeparator()); // separate
-        foldingShape = new JMenu("Shape");
-        foldingMenu.add(foldingShape); // foldShape -> foldMenu
-        foldingFoldShapes = new JMenuItem("Fold Shapes");
-        foldingFoldShapes.addActionListener(this);
-        foldingShape.add(foldingFoldShapes);
-        foldingConnectShapes = new JMenuItem("Connect Shapes");
-        foldingConnectShapes.addActionListener(this);
-        foldingShape.add(foldingConnectShapes);
-        foldingDetachShapes = new JMenuItem("Detach Shapes");
-        foldingDetachShapes.addActionListener(this);
-        foldingShape.add(foldingDetachShapes);
-        foldingResizeShape = new JMenuItem("Resize shape");
-        foldingResizeShape.addActionListener(this);
-        foldingShape.add(foldingResizeShape);
-
-        // Window submenu
-        windowMenu = new JMenu("Window");
-        menuBar.add(windowMenu);// winMenu -> menuBar
-        windowView = new JMenu("View");
-        windowMenu.add(windowView); // winView -> winMenu
-        windowShowTop = new JCheckBoxMenuItem("Show top", true);
-        windowShowTop.addActionListener(this);
-        windowView.add(windowShowTop);
-        windowShowBack = new JCheckBoxMenuItem("Show Back", true);
-        windowShowBack.addActionListener(this);
-        windowView.add(windowShowBack);
-        windowShowLeft = new JCheckBoxMenuItem("Show Left", true);
-        windowShowLeft.addActionListener(this);
-        windowView.add(windowShowLeft);
-        windowMenu.add(new JSeparator()); // separate
-        windowShowHideTools = new JMenuItem("Show/Hide Tools");
-        windowShowHideTools.addActionListener(this);
-        windowMenu.add(windowShowHideTools);
-        windowShowHideInfo = new JMenuItem("Show/Hide Information Panel");
-        windowShowHideInfo.addActionListener(this);
-        windowMenu.add(windowShowHideInfo);
-        windowMenu.add(new JSeparator()); // separate
-        windowPerspective = new JMenu("Perspective");
-        windowMenu.add(windowPerspective); // winPersp -> winMenu
-        windowChangePerspective = new JMenuItem("Change Perspective Layout");
-        windowChangePerspective.addActionListener(this);
-        windowPerspective.add(windowChangePerspective);
-        windowSavePerspective = new JMenuItem("Save Perspective Layout");
-        windowSavePerspective.addActionListener(this);
-        windowPerspective.add(windowSavePerspective);
-        windowLoadPerspective = new JMenuItem("Load Perspective Layout");
-        windowLoadPerspective.addActionListener(this);
-        windowPerspective.add(windowLoadPerspective);
-        windowResizePerspective = new JMenuItem("Resize Perspective");
-        windowResizePerspective.addActionListener(this);
-        windowPerspective.add(windowResizePerspective);
-
-        // Help menu items
-        helpMenu = new JMenu("Help");
-        menuBar.add(helpMenu); // helpMenu -> menuBar
-        helpManual = new JMenuItem("Manual");
-        helpManual.addActionListener(this);
-        helpMenu.add(helpManual);
-        helpQuickStartGuide = new JMenuItem("Quick Start Guide");
-        helpQuickStartGuide.addActionListener(this);
-        helpMenu.add(helpQuickStartGuide);
-
-        helpLanguage = new JMenu("Languages");
-        helpMenu.add(helpLanguage);
-        liLanguages = new ArrayList<>();
-        langGroup = new ButtonGroup();
-        for (String s : MessagesUtils.getInstance().getDisplayedLanguages()) {
-            JRadioButtonMenuItem jrbmi = new JRadioButtonMenuItem(s, false);
-            jrbmi.addActionListener(this);
-            if (s.equals(Messages.getLocale().getDisplayLanguage(Messages.getLocale()))) {
-                jrbmi.setSelected(true);
-            }
-            liLanguages.add(jrbmi);
-            helpLanguage.add(jrbmi);
-            langGroup.add(jrbmi);
-        }
-
     }
 
     /**
@@ -485,7 +256,7 @@ public final class GUI extends JFrame
      */
     public void initPanesAndGui() {
 
-        initMenuBarPane();
+        menuBar = new MenuBar(this);
         setJMenuBar(menuBar);
 
         initPalettePane();
@@ -674,47 +445,7 @@ public final class GUI extends JFrame
      */
     private void label() {
 
-        // menu bar
-        // file submenu
-        fileMenu.setText(Messages.getString(Messages.FI));
-        fileOpen.setText(Messages.getString(Messages.FI + ".open"));
-        fileNew.setText(Messages.getString(Messages.FI + ".new"));
-        fileSave.setText(Messages.getString(Messages.FI + ".save"));
-        fileSaveAs.setText(Messages.getString(Messages.FI + ".saveas"));
-        fileExport.setText(Messages.getString(Messages.FI + ".export"));
-        fileClose.setText(Messages.getString(Messages.FI + ".close"));
-        // edit submenu
-        editMenu.setText(Messages.getString(Messages.ED));
-        editCopy.setText(Messages.getString(Messages.ED + ".copy"));
-        editCut.setText(Messages.getString(Messages.ED + ".cut"));
-        editPaste.setText(Messages.getString(Messages.ED + ".paste"));
-        editDelete.setText(Messages.getString(Messages.ED + ".delete"));
-        // folding submenu
-        foldingMenu.setText(Messages.getString(Messages.FO));
-        foldingThirty.setText(Messages.getString(Messages.FO + ".angle.30"));
-        foldingFortyFive.setText(Messages.getString(Messages.FO + ".angle.45"));
-        foldingNinety.setText(Messages.getString(Messages.FO + ".angle.90"));
-        foldingCustomAngle.setText(Messages.getString(Messages.FO + ".angle.custom"));
-        foldingEdgeSelect.setText(Messages.getString(Messages.FO + ".edgesel"));
-        foldingPointSelect.setText(Messages.getString(Messages.FO + ".pointsel"));
-        foldingFoldShapes.setText(Messages.getString(Messages.FO + ".shape.fold"));
-        foldingConnectShapes.setText(Messages.getString(Messages.FO + ".shape.connect"));
-        foldingDetachShapes.setText(Messages.getString(Messages.FO + ".shape.detach"));
-        // window submenu
-        windowMenu.setText(Messages.getString(Messages.WI));
-        windowShowTop.setText(Messages.getString(Messages.WI + ".view.top"));
-        windowShowBack.setText(Messages.getString(Messages.WI + ".view.back"));
-        windowShowLeft.setText(Messages.getString(Messages.WI + ".view.left"));
-        windowShowHideInfo.setText(Messages.getString(Messages.WI + ".info"));
-        windowShowHideTools.setText(Messages.getString(Messages.WI + ".tools"));
-        windowChangePerspective.setText(Messages.getString(Messages.WI + ".persp.change"));
-        windowSavePerspective.setText(Messages.getString(Messages.WI + ".persp.save"));
-        windowResizePerspective.setText(Messages.getString(Messages.WI + ".persp.resize"));
-        // help submenu
-        helpMenu.setText(Messages.getString(Messages.HE));
-        helpManual.setText(Messages.getString(Messages.HE + ".manual"));
-        helpQuickStartGuide.setText(Messages.getString(Messages.HE + ".guide"));
-        helpLanguage.setText(Messages.getString(Messages.HE + ".language"));
+        menuBar.label();
 
         // panels titles FIXME make it work.
         ((TitledBorder) freeViewPanel.getBorder()).setTitle(Messages.getString("panels.freeview"));
